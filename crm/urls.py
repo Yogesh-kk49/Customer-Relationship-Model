73d9c959c1555b76a5228d15ae395c62b402cbcb
Yogesh <yogeshkumarkk49@gmail.com>
2026-01-25 19:58:20 +0530

    Fix landing page render

--- crm/urls.py
+++ crm/urls.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
 
 def home(request):
-    return redirect("home.html")
+    return render(request, "home.html")  # ✅ CORRECT
 
 urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("documents/", include("documents.urls")),
-    
 ]
 
 if settings.DEBUG:
